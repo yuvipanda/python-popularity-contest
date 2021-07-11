3da2638c3c1a027a2f69f23737da10826e93b6ea
YuviPanda <yuvipanda@gmail.com>
2021-07-11 19:41:05 +0530

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='popularity-contest',
-    version='0.4',
+    version='0.4.1',
     url="https://github.com/yuvipanda/python-popularity-contest",
     description="Privacy-friendly data collection of the libraries your users are using",
     long_description=open('README.md').read(),
